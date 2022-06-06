218d0d3ea3645b0ba730ddb9bf97449a45c0da25
Matthew Gilligan <matthewjgilligan11@gmail.com>
2022-06-06 15:25:24 -0700

    Working on pathfinding, ui still broken

--- Assets/Scripts/Nodes/BaseNode.cs
+++ Assets/Scripts/Nodes/BaseNode.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseNode : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class BaseNode { 
 
-    // Update is called once per frame
-    void Update()
-    {
+    public BaseNode Connection { get; private set; }
+    public float G { get; private set; }
+    public float H { get; private set; }
+    public float F => G + H; //lambda
+
+
+    public void SetConnection(BaseNode baseNode) => Connection = baseNode;
+
+
+    public void SetG(float g) => G = g; //sets the variable G to the new g
+
+    public void SetH(float h) => H = h; 
+
         
-    }
+    
+   
 }
